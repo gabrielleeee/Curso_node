237164809f954e8992f973930806a402b3b95f2d
gabrielleeee <gabi.poletti2019@gmail.com>
2024-02-01 16:05:20 -0300

    Add seção 10 mvc

---
+++ 10_MVC/1_estrutura/db/conn.js
@@ -0,0 +1,15 @@
+const { Sequelize } = require('sequelize')
+
+const sequelize = new Sequelize('nodemvc1', 'root', '', {
+  host: 'localhost',
+  dialect: 'mysql'  //qual banco eu quero integrar
+})
+
+// try {
+//   sequelize.authenticate()
+//   console.log('Conectamos com sucesso com o Sequelize!')
+// } catch (err) {
+//   console.log('Não foi possível conectar: ', error)
+// }
+
+module.exports = sequelize
